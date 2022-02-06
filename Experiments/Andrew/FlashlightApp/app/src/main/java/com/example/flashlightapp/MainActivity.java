66c8261cf1c027cf5be40f503541f752b02c5057
Andrew Chiang <andrew3@iastate.edu>
2022-02-06 01:40:36 -0600

    added function to turn on and off camera flashlight and changed the image name to lowecase

--- Experiments/Andrew/FlashlightApp/app/src/main/java/com/example/flashlightapp/MainActivity.java
+++ Experiments/Andrew/FlashlightApp/app/src/main/java/com/example/flashlightapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.flashlightapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -18,6 +23,7 @@ import com.karumi.dexter.listener.single.PermissionListener;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton imageButton;
+    boolean flashlightState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
             @Override
             public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+            runFlashlight();
             }
 
             @Override
@@ -44,5 +51,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void runFlashlight() {
 
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                if(!flashlightState){
+                    CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                    try {
+                        String cameraId= cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraId, true);
+                        flashlightState = true;
+                        imageButton.setImageResource(R.drawable.Flashlight_ON); //changes the Flashlight Image to ON
+                    }
+                    catch (CameraAccessException e) {}
+                } else {
+                    CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                    try {
+                        String cameraId= cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraId, false);
+                        flashlightState = false;
+                        imageButton.setImageResource(R.drawable.Flashlight_OFF); //changes the Flashlight Image to OFF
+                    }
+                    catch (CameraAccessException e) {}
+                }
+            }
+        });
+    }
 }
