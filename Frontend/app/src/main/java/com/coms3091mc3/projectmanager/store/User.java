238675e82519a84868c1b4c4a9145ad0ab372758
Zeyu Yang <zeyuyang@iastate.edu>
2022-02-19 16:41:32 -0600

    feat: init data binding

---
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/store/User.java
@@ -0,0 +1,13 @@
+package com.coms3091mc3.projectmanager.store;
+
+public class User {
+    private final String username;
+
+    public User() {
+        this.username = "UNKNOWN";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
