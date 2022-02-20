2de773e98824ab25f8beac0699a1b6f6ae4c6964
Andrew Chiang <andrew3@iastate.edu>
2022-02-19 20:52:06 -0600

    added user data base

---
+++ Backend/userDB/src/test/java/UserDB/UserDbApplicationTests.java
@@ -0,0 +1,13 @@
+package UserDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
