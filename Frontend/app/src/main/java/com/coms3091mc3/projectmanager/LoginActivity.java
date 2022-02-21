34d1f8bdb472c756ff88da72d8f27e41860ea555
On Wave Tiong <Wave@mac-air-068.local>
2022-02-21 03:06:51 -0600

    Login page design

---
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.coms3091mc3.projectmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/utils/Const.java
@@ -0,0 +1,8 @@
+package com.coms3091mc3.projectmanager.utils;
+
+public class Const {
+    public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
+    public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+    public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
+    public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+}
