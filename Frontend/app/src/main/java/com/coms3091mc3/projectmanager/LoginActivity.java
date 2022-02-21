84ba8f8b5c04a09fd7bd1996a7738a5c838ffc94
On Wave Tiong <Wave@mac-air-068.local>
2022-02-21 10:16:26 -0600

    Added Volley Requests to Postman mock server

--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/LoginActivity.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/LoginActivity.java
@@ -3,12 +3,80 @@ package com.coms3091mc3.projectmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.coms3091mc3.projectmanager.app.AppController;
+import com.coms3091mc3.projectmanager.utils.Const;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
+    StringRequest loginRequest;
+    String tag_login_req = "login_req";
+    String url;
+    EditText username, password;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btnLogin);
+        ProgressBar pBar = findViewById(R.id.progressBar);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                pBar.setVisibility(View.VISIBLE);
+                url = Const.MOCK_SERVER;
+                url+="/login";
+//                url = "https://api.androidhive.info/volley/string_response.html";
+                loginRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                            Log.d("login_debug", response.toString());
+
+                            pBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(getApplicationContext(),response.toString(), Toast.LENGTH_LONG).show();
+                    }
+                },new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d("login_debug_error","Error: "+ error.toString());
+                        // hide the progress dialog
+                        pBar.setVisibility(View.INVISIBLE);
+                    }
+                }) {
+                    @Override
+                    public Response<String> parseNetworkResponse(NetworkResponse response) {
+                        int mStatusCode = response.statusCode;
+                        Log.d("login_debug_status",String.valueOf(mStatusCode));
+                        return super.parseNetworkResponse(response);
+                    };
+                    //for POST method
+//                    @Override
+//                    protected Map<String,String> getParams(){
+//                        Map<String,String> params = new HashMap<String, String>();
+//                        params.put("username",username.getText().toString());
+//                        params.put("password",password.getText().toString());
+//                        return params;
+//                    };
+                };
+                AppController.getInstance().addToRequestQueue(loginRequest, tag_login_req);
+            }
+        });
     }
 }
--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/utils/Const.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/utils/Const.java
@@ -1,8 +1,9 @@
 package com.coms3091mc3.projectmanager.utils;
 
 public class Const {
-    public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
-    public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
-    public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
-    public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+//    public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
+//    public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+//    public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
+//    public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+     public static final String MOCK_SERVER = "https://bd9f22ed-10c9-4c41-a415-b951634333f6.mock.pstmn.io";
 }
