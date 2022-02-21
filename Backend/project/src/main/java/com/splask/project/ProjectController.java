ae1e30ede9e38a92d75eb37f1814f2db787f77c8
Chad Monmany <cmonmany@iastate.edu>
2022-02-20 18:28:07 -0600

    basic setup and check of project Database.

--- Backend/project/src/main/java/com/splask/project/ProjectController.java
+++ Backend/project/src/main/java/com/splask/project/ProjectController.java
@@ -4,12 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProjectController {
 
     @Autowired
     projectDB db;
 
+    @GetMapping("/project/{id}")
+    Project getProject(@PathVariable Integer id)
+    {
+        return db.findById(id).
+                orElseThrow(RuntimeException::new);
+    }
 
+    @RequestMapping("/projects")
+    List<Project> hello(){return db.findAll();}
 
 }
