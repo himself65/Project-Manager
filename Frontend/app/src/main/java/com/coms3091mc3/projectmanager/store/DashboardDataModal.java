f0060ad68de93128e5c319ebca74753e9871b731
Zeyu Yang <zeyuyang@iastate.edu>
2022-02-25 09:40:50 -0600

    feat: add button in dashboard page

--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/store/DashboardDataModal.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/store/DashboardDataModal.java
@@ -1,6 +1,7 @@
 package com.coms3091mc3.projectmanager.store;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import androidx.databinding.ObservableField;
@@ -11,7 +12,10 @@ public class DashboardDataModal {
     public ObservableField<String> name;
     public ArrayAdapter<String> projectsAdapter;
 
+    private Context context;
+
     public DashboardDataModal(Context context) {
+        this.context = context;
         this.name = new ObservableField<>("UNKNOWN");
         this.projectsAdapter = new ArrayAdapter<>(context, R.layout.fragment_item, R.id.content);
     }
