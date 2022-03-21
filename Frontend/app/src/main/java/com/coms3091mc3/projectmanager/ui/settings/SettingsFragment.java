cea1537f5895621e6c3735f21b3d4d66e051a7b6
Zeyu Yang <zeyuyang@iastate.edu>
2022-03-21 14:50:27 -0500

    fix: menu button on setting fragment

--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/settings/SettingsFragment.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/settings/SettingsFragment.java
@@ -11,20 +11,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.coms3091mc3.projectmanager.databinding.FragmentHomeBinding;
+import com.coms3091mc3.projectmanager.databinding.FragmentSettingsBinding;
 import com.coms3091mc3.projectmanager.ui.home.HomeViewModel;
 
 public class SettingsFragment extends Fragment {
-    private FragmentHomeBinding binding;
+    private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SettingsViewModel homeViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        final TextView textView = binding.textSettings;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
