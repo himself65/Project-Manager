7ec5304f84d57bc1c366993ac18ef464eb8e497d
Zeyu Yang <zeyuyang@iastate.edu>
2022-04-05 15:09:47 -0500

    feat: load team

--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/store/TeamDataModel.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/store/TeamDataModel.java
@@ -1,7 +1,13 @@
 package com.coms3091mc3.projectmanager.store;
 
-public class TeamDataModel {
-    public TeamDataModel() {
+import androidx.databinding.ObservableField;
 
+import com.coms3091mc3.projectmanager.data.Team;
+
+public class TeamDataModel {
+    public ObservableField<Team> team;
+
+    public TeamDataModel() {
+        this.team = new ObservableField<>(new Team(0, "UNKNOWN"));
     }
 }
--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/team/TeamFragment.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/team/TeamFragment.java
@@ -9,8 +9,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.coms3091mc3.projectmanager.app.AppController;
+import com.coms3091mc3.projectmanager.data.Team;
 import com.coms3091mc3.projectmanager.databinding.FragmentTeamBinding;
 import com.coms3091mc3.projectmanager.store.TeamDataModel;
+import com.coms3091mc3.projectmanager.utils.Const;
+
+import org.json.JSONException;
 
 public class TeamFragment extends Fragment {
     FragmentTeamBinding binding;
@@ -20,6 +27,28 @@ public class TeamFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentTeamBinding.inflate(inflater, container, false);
         binding.setModal(new TeamDataModel());
+        int id = (Integer) getArguments().get("teamID");
+        teamRequest(id);
         return binding.getRoot();
+    }
+
+    void teamRequest(int id) {
+        String url = Const.API_SERVER + "/team/" + id;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                object -> {
+                    try {
+                        String teamName = object.getString("teamName");
+                        int teamID = object.getInt("teamID");
+                        Team team = new Team(teamID, teamName);
+                        binding.getModal().team.set(team);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+
+                }
+        );
+        AppController.getInstance().addToRequestQueue(request);
     }
 }
