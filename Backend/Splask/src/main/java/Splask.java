8060078767a0b9381f366897cec449869e87ca31
cmonmany <cmonmany@iastate.edu>
2022-04-13 10:29:54 -0500

    Update Splask.java

--- Backend/Splask/src/main/java/Splask.java
+++ Backend/Splask/src/main/java/Splask.java
@@ -1,11 +1,16 @@
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 
-
-@SpringBootApplication(scanBasePackages = {"com.splask.Services"})
+@SpringBootApplication(scanBasePackages = {"com.splask"})
+@ComponentScan({"com.splask"})
+@EntityScan("com.splask.Models")
+@EnableJpaRepositories("com.splask.Repositories")
 @OpenAPIDefinition
 public class Splask {
 
