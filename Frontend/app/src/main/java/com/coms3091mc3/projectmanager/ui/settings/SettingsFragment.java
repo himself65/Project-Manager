3db9791bd1eb3bfdfafcc3f1bd114b2577894d87
Zeyu Yang <zeyuyang@iastate.edu>
2022-04-29 21:20:55 -0500

    feat: init theme support

--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/settings/SettingsFragment.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/settings/SettingsFragment.java
@@ -28,8 +28,6 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSettings;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
