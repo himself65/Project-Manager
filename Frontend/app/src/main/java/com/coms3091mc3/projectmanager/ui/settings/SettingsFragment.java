2c64220d56e105c7dbc483ad57845eae99600b55
Zeyu Yang <zeyuyang@iastate.edu>
2022-05-02 16:53:27 -0500

    feat: add request to put image

--- Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/settings/SettingsFragment.java
+++ Frontend/app/src/main/java/com/coms3091mc3/projectmanager/ui/settings/SettingsFragment.java
@@ -1,8 +1,10 @@
 package com.coms3091mc3.projectmanager.ui.settings;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,18 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.coms3091mc3.projectmanager.R;
 import com.coms3091mc3.projectmanager.databinding.FragmentSettingsBinding;
 import com.coms3091mc3.projectmanager.store.SettingDataModal;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,11 +37,31 @@ import java.util.logging.Logger;
 public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
 
+
     ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
             new ActivityResultCallback<Uri>() {
                 @Override
                 public void onActivityResult(Uri uri) {
                     ImageButton button = binding.getRoot().findViewById(R.id.avatar);
+                    byte[] bytes = getBytesArrayFromURI(uri);
+                    JSONArray array = new JSONArray();
+                    try {
+                        for (byte b : bytes) {
+                            array.put(b);
+                        }
+                        JSONObject data = new JSONObject();
+                        data.put("image", array);
+                        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, "someAPI", data,
+                                response -> {
+                                    // todo
+                                },
+                                error -> {
+                                    // todo
+                                }
+                        );
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     button.setImageURI(uri);
                 }
             });
@@ -51,6 +81,27 @@ public class SettingsFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public byte[] getBytesArrayFromURI(Uri uri) {
+        try {
+            ContentResolver resolver = getContext().getContentResolver();
+            InputStream inputStream = resolver.openInputStream(uri);
+            ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+
+            int len = 0;
+            while ((len = inputStream.read(buffer)) != -1) {
+                byteBuffer.write(buffer, 0, len);
+            }
+
+            return byteBuffer.toByteArray();
+
+        } catch (Exception e) {
+            Log.d("exception", "Oops! Something went wrong.");
+        }
+        return null;
     }
 
     @Override
