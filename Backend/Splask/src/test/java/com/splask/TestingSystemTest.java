9dd75ad1388e2f3e0409a81e301dcf9a93eee250
Andrew Chiang <andrew3@iastate.edu>
2022-05-03 19:39:08 -0500

    commented stuff so Spring would run

--- Backend/Splask/src/test/java/com/splask/TestingSystemTest.java
+++ Backend/Splask/src/test/java/com/splask/TestingSystemTest.java
@@ -26,26 +26,26 @@ public class TestingSystemTest {
         RestAssured.baseURI = "http://localhost";
     }
 
-
-    @Test
-    public void registerUserTest() {
-        // Send request and receive response
-        Response response = RestAssured.given().
-                body(). //parameters that we are testing, eg. name, id,
-                when().
-                post(); //endpoint
-
-        // Check status code
-        int statusCode = response.statusCode();
-        // The status code varies depending on the call, if its a PUT, DELETE, GET, POST, etc
-        assertEquals(200, statusCode);
-
-
-
-
-
-
-    }
+//
+//    @Test
+//    public void registerUserTest() {
+//        // Send request and receive response
+//        Response response = RestAssured.given().
+//                body(). //parameters that we are testing, eg. name, id,
+//                when().
+//                post(); //endpoint
+//
+//        // Check status code
+//        int statusCode = response.statusCode();
+//        // The status code varies depending on the call, if its a PUT, DELETE, GET, POST, etc
+//        assertEquals(200, statusCode);
+//
+//
+//
+//
+//
+//
+//    }
 
 
 
